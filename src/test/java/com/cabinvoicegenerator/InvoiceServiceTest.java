c663486b819700454a067ba2c3bdbc73cd701ec6
HarithaRugada <haritharugada@gmail.com>
2020-07-10 19:35:38 +0530

    ADDED : Test case to get total fare for multiple rides

--- src/test/java/com/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/cabinvoicegenerator/InvoiceServiceTest.java
@@ -1,6 +1,7 @@
 package com.cabinvoicegenerator;
 
 import com.cabinvoicegenerator.service.InvoiceGenerator;
+import com.cabinvoicegenerator.utility.Ride;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,5 +22,15 @@ public class InvoiceServiceTest {
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
